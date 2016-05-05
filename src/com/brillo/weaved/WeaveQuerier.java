aa4f1116ec132ecafb4ea4e2ce3276b126bf61a1
Ameya <achikodi@clearblade.com>
2016-05-04 21:23:03 -0500

    mid commit

--- src/com/brillo/weaved/WeaveQuerier.java
+++ src/com/brillo/weaved/WeaveQuerier.java
@@ -1,35 +1,25 @@
 package com.brillo.weaved;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
-import java.util.Map;
 
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.clouddevices.CloudDevices.Devices;
 import com.google.api.services.clouddevices.model.Device;
 
 public class WeaveQuerier {
-	public static void main(String[] args) throws IOException, SQLException, ParseException {
-		 CloudDevicesSample cloud= new CloudDevicesSample();
-		 List<Device>   devices = cloud.run();
-
-for(Device dev:devices){
-	System.out.println(dev.getId());
-	System.out.println(dev.getUnknownKeys().get("name").toString());
-	checkEvent(dev);
 	
+public WeaveQuerier(List<Device> devices) throws SQLException, ParseException, IOException {
+	eventEvoker(devices);
+}
+	public static void eventEvoker(List<Device> devices) throws SQLException, ParseException, IOException{
+		for(Device dev:devices){
+			checkEvent(dev);
+		}
 	}
-	}
+	
 	public static void checkEvent(Device dev) throws SQLException, ParseException, IOException{
 
 		if(dev.getUnknownKeys().get("name").toString().contentEquals("weave_daemon_ledflasher")){
@@ -38,7 +28,8 @@ for(Device dev:devices){
 			JSONObject action= led.LEDeventHandler(dev.getState(),dev.getId());
 			System.out.println(action);
 			if(action!=null){
-			String actionDev=(String)action.get("Device");
+			String actionDev=(String)action.get("DeviceName");
+			System.out.println(actionDev);
 			checkAction(actionDev,action);
 			}
 		}
@@ -46,6 +37,8 @@ for(Device dev:devices){
 			
 		}
 	}
+	
+	
 	public static void checkAction(String actionDev,JSONObject action) throws IOException, SQLException{
 		if(actionDev.contentEquals("LED")){
 			LED led = new LED();
@@ -58,4 +51,3 @@ for(Device dev:devices){
 	}
 
 }
-
--- src/com/brillo/weaved/WeavedServlet.java
+++ src/com/brillo/weaved/WeavedServlet.java
@@ -1,10 +1,10 @@
 package com.brillo.weaved;
 
 import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -12,11 +12,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.googleapis.services.CommonGoogleClientRequestInitializer;
 import com.google.api.client.http.javanet.NetHttpTransport;
@@ -24,10 +24,7 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.clouddevices.CloudDevices;
 import com.google.api.services.clouddevices.model.Device;
 import com.google.api.services.clouddevices.model.DevicesListResponse;
-
 import com.mysql.jdbc.PreparedStatement;
-
-
 
 
 /**
@@ -35,52 +32,39 @@ import com.mysql.jdbc.PreparedStatement;
  */
 
 public class WeavedServlet extends HttpServlet {
-  
+
 	private static final long serialVersionUID = 1L;
-
-	private final NetHttpTransport httpTransport = new NetHttpTransport();
-	private static final JacksonFactory jsonFactory = new JacksonFactory();
-	
-	  
-	
-	
-	// Redirect URL for client side installed apps.
-	
-	  // See https://developers.google.com/weave/v1/dev-guides/getting-started/authorizing#setup
-	  // on how to set up your project and obtain client ID, client secret and API key.
-	/* private static final String CLIENT_ID = "593065169550-8o2r95tnqqtqmfgsgv5qn1e1u16hdr0h.apps.googleusercontent.com";
-	  private static final String CLIENT_SECRET = "NtViFQ_4_urrOvv_I1Cex6d2";*/
-	  
-	  //private static final String AUTH_SCOPE = "https://www.googleapis.com/auth/weave.app";
-
-	  // Redirect URL for client side installed apps.
-	  private static final String REDIRECT_URL = "http://localhost:8888/";
-	  
-	  
-	  
-
-
 	
 	// See https://developers.google.com/weave/v1/dev-guides/getting-started/authorizing#setup
-	  // on how to set up your project and obtain client ID, client secret and API key.
-	 private static final String CLIENT_ID = "309435708548-fsvfu060n29531ufr5qqqgf7t2jhhvan.apps.googleusercontent.com";
-	 private static final String CLIENT_SECRET = "oFWicANTZPjRLdyC-gcJZdw-";
-	 private static final String API_KEY = "AIzaSyDaR-PSdRK0psv1ZyDcaY2n2UFpZWui2UE";
+	// on how to set up your project and obtain client ID, client secret and API key.
+	private static final String CLIENT_ID = "309435708548-1gdb132v9q00p53kbs7eot6i7abtt0n1.apps.googleusercontent.com";
+	private static final String CLIENT_SECRET = "kemQzrbauxkeLV_fSGWivKgW";
+	private static final String API_KEY = "AIzaSyDaR-PSdRK0psv1ZyDcaY2n2UFpZWui2UE";
+	
+	// Redirect URL for client side installed apps.
+	private static final String REDIRECT_URL = "http://localhost:8888";
 
-	 private static String Access_Token;
-	 private static String Refresh_Token;
-	 private static String Id_Token;
-	 
-	 private String userId= "111612014973784390146";
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public WeavedServlet() {
-        super();
-        // TODO Auto-generated constructor stubksonFactory;
-    }
+	private static String Access_Token;
+	private static String Auth_Code;
+	private static String Refresh_Token;
+	private static String Id_Token;
+	public  static String userId;
+	private static String userEmail;
+	
+	public static final NetHttpTransport httpTransport = new NetHttpTransport();
+	public static final JacksonFactory jsonFactory = new JacksonFactory();
+	
+	CloudDevices apiClient;
 
-    
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public WeavedServlet() {
+		super();
+		// TODO Auto-generated constructor stubksonFactory;
+	}
+
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -88,114 +72,182 @@ public class WeavedServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		//response.getWriter().println("in servlet");
-		
-		String authCode = request.getReader().readLine();
+		String BrowserQuery;
+
+		BrowserQuery =  request.getReader().readLine();
+
+		String[] BrowserQueryResponse = BrowserQuery.split("#");
 		//response.getWriter().println(authCode);
-		System.out.println("Authcode:"+authCode);
-		
-		CloudDevices apiClient = getApiClient(authCode);
+		//System.out.println("Authcode:"+authCode);
+
+		userEmail = BrowserQueryResponse[0];
+		Auth_Code = BrowserQueryResponse[1];
+
+		System.out.println(userEmail);
+		apiClient = getApiClient(Auth_Code);
 		try {
-			SQLConnectorURLgenerator();
+			userId = IdVerifier();
+			credentialsStore();
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-	    DevicesListResponse devicesListResponse;
-		devicesListResponse = apiClient.devices().list().execute();
-		List<Device> devices = devicesListResponse.getDevices();
-	    
-		int i = 0;
-	    for(Device dev : devices){
-	        System.out.println("Available device "+ ++i+": " + dev.getId());
-	    }
-	    response.sendRedirect("http://localhost:8888");
-	    
-	}
-	
-
-	private CloudDevices getApiClient(String authCode) throws IOException {
-	    // Try to load cached credentials.
-	    GoogleCredential credential=null;
-	    if (credential == null) {
-	      System.out.println("Did not find cached credentials");
-	     credential = authorize(authCode);
-	    }
-	    return new CloudDevices.Builder(httpTransport, jsonFactory, credential)
-	        .setApplicationName("Weave Sample")
-	        .setServicePath("clouddevices/v1")
-	        .setGoogleClientRequestInitializer(new CommonGoogleClientRequestInitializer(API_KEY))
-	        .build();
-	  }
-	
-	  /**
-	   * Goes through Google OAuth2 authorization flow. See more details:
-	   * https://developers.google.com/weave/v1/dev-guides/getting-started/authorizing
-	 * @return 
-	   */
-	  private GoogleCredential authorize(String authCode) throws IOException {
-		  String authorizationCode = authCode;
-		  System.out.println("test");
-		  // Use the authorization code to get an access token and a refresh token.
-		  GoogleAuthorizationCodeTokenRequest request = new GoogleAuthorizationCodeTokenRequest(
-	        httpTransport, JacksonFactory.getDefaultInstance(), CLIENT_ID, CLIENT_SECRET, authorizationCode,
-	        REDIRECT_URL);
-	    System.out.println(request);
-	    GoogleTokenResponse response = request.execute();
-	    System.out.println(response);
-	    Access_Token=response.getAccessToken();
-	    Refresh_Token=response.getRefreshToken();
-	    Id_Token=response.getIdToken();
-	    
-	    System.out.println("Refresh TOken:"+response.getRefreshToken());
-	    System.out.println("Id TOken:"+response.getIdToken());
-	    // Use the access and refresh tokens to set up credentials.
-	    GoogleCredential credential = new GoogleCredential.Builder()
-	        .setJsonFactory(jsonFactory)
-	        .setTransport(httpTransport)
-	        .setClientSecrets(CLIENT_ID, CLIENT_SECRET)
-	        .build()
-	        .setFromTokenResponse(response);
-	    return credential;
-	  }
-	
-	  
-	  public static void SQLConnectorURLgenerator() throws ClassNotFoundException {
-			String url;
-			String user = null;
-			String password = null;
-			
-		Class.forName("com.mysql.jdbc.Driver");
-			url = "jdbc:mysql://127.0.0.1:3306/Credentials";
-			user 		= "root";
-			password 	= "root";			
-		  getConnection(url, user, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (GeneralSecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
-		private static void getConnection(String url, String user, String password){
-			System.out.println(user+"    "+password);
-			String type = "Unauthorized_user";
-			try {
-				Connection conn =  DriverManager.getConnection(url,user,password);
-				
-				String statement = "INSERT INTO Credentials ( client_id, client_secret, refresh_token, type ) VALUES(?,?,?,?) ";
-		          PreparedStatement stmt =  (PreparedStatement) conn.prepareStatement(statement);
-		          stmt.setString(1, CLIENT_ID);
-		          stmt.setString(2, CLIENT_SECRET);
-		          stmt.setString(3, Refresh_Token);
-		          stmt.setString(4, type);
-		          int success = 2;
-		          success = stmt.executeUpdate();
-		          if (success == 1) {
-		            System.out.println("Success! Database written");
-		          } else if (success == 0) {
-		            System.out.println("Failure! Database not written");
-		          }
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		//receiveDeviceList();
+		
+		//	    response.sendRedirect("http://localhost:8888");
+
+		//WeaveQuerier query = new WeaveQuerier();
+		
+		/*DeviceStatus ds = new DeviceStatus();
+		try {
+			ds.SQLFetchUser();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+	}
+
+
+	private CloudDevices getApiClient(String Access_Token) throws IOException {
+		// Try to load cached credentials.
+		GoogleCredential credential=null;
+		if (credential == null) {
+			System.out.println("Did not find cached credentials");
+			credential = authorize(Auth_Code);
 		}
-	  
+		return new CloudDevices.Builder(httpTransport, jsonFactory, credential)
+				.setApplicationName("Weave Sample")
+				.setServicePath("clouddevices/v1")
+				.setGoogleClientRequestInitializer(new CommonGoogleClientRequestInitializer(API_KEY))
+				.build();
+	}
+
+	/**
+	 * Goes through Google OAuth2 authorization flow. See more details:
+	 * https://developers.google.com/weave/v1/dev-guides/getting-started/authorizing
+	 * @return 
+	 */
+	private GoogleCredential authorize(String Auth_Code) throws IOException {
+		String authorizationCode = Auth_Code;
+		System.out.println("test");
+		// Use the authorization code to get an access token and a refresh token.
+		GoogleAuthorizationCodeTokenRequest request = new GoogleAuthorizationCodeTokenRequest(
+				httpTransport, JacksonFactory.getDefaultInstance(), CLIENT_ID, CLIENT_SECRET, authorizationCode,
+				REDIRECT_URL);
+		//System.out.println(request);
+		GoogleTokenResponse response = request.execute();
+		//System.out.println(response);
+		Access_Token=response.getAccessToken();
+		Refresh_Token=response.getRefreshToken();
+		Id_Token=response.getIdToken();
+
+		System.out.println("Refresh TOken:"+response.getRefreshToken());
+		System.out.println("Id TOken:"+response.getIdToken());
+		// Use the access and refresh tokens to set up credentials.
+		GoogleCredential credential = new GoogleCredential.Builder()
+				.setJsonFactory(jsonFactory)
+				.setTransport(httpTransport)
+				.setClientSecrets(CLIENT_ID, CLIENT_SECRET)
+				.build()
+				.setFromTokenResponse(response);
+		return credential;
+
+		/*GoogleCredential credential = new GoogleCredential();
+		  credential.setAccessToken(Access_Token);
+		  return credential;*/
+	}
+
+	
+	private String IdVerifier() throws GeneralSecurityException, IOException{
+		String userid = null;
+		GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(httpTransport, jsonFactory)
+				.setAudience(Arrays.asList(CLIENT_ID))
+				// If you retrieved the token on Android using the Play Services 8.3 API or newer, set
+				// the issuer to "https://accounts.google.com". Otherwise, set the issuer to
+				// "accounts.google.com". If you need to verify tokens from multiple sources, build
+				// a GoogleIdTokenVerifier for each issuer and try them both.
+				.setIssuer("accounts.google.com")
+				.build();
+
+		// (Receive idTokenString by HTTPS POST)
+
+		GoogleIdToken idToken;
+		idToken = verifier.verify(Id_Token);
+
+		if (idToken != null) {
+			Payload payload = idToken.getPayload();
+			// Print user identifier
+			userid = payload.getSubject();
+			System.out.println("User ID: " + userid);
+
+			// Get profile information from payload
+			/*String email = payload.getEmail();
+			boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+			String name = (String) payload.get("name");
+			String pictureUrl = (String) payload.get("picture");
+			String locale = (String) payload.get("locale");
+			String familyName = (String) payload.get("family_name");
+			String givenName = (String) payload.get("given_name");*/
+
+			// Use or store profile information
+			// ...
+
+		} else {
+			System.out.println("Invalid ID token.");
+		}
+		return userid;
+	}
+
+	
+	private void credentialsStore() throws SQLException, ClassNotFoundException{
+		String databaseName = "Credentials";
+		String user_type = "Authorized_User";
+		SQLConnector sql = new SQLConnector();
+		Connection conn;
+
+		conn = sql.SQLConnectorURLgenerator(databaseName);
+		String statement = "INSERT INTO Credentials ( UserId, access_token, refresh_token, id_token, user_type ) VALUES(?,?,?,?,?) "
+				+ "ON DUPLICATE KEY UPDATE access_token = ?";
+		System.out.println(statement);
+		PreparedStatement stmt =  (PreparedStatement) conn.prepareStatement(statement);
+		stmt.setString(1, userId);
+		stmt.setString(2, Access_Token);
+		stmt.setString(3, Refresh_Token);
+		stmt.setString(4, Id_Token);
+		stmt.setString(5, user_type);
+		stmt.setString(6, Access_Token);
+		int success = 2;
+		success = stmt.executeUpdate();
+		if (success == 1) {
+			System.out.println("Success! Database written");
+		} else if (success == 0) {
+			System.out.println("Failure! Database not written");
+		}
+	}
+	
+	public List<Device> receiveDeviceList() throws IOException{
+		DevicesListResponse devicesListResponse;
+		devicesListResponse = apiClient.devices().list().execute();
+		List<Device> devices = devicesListResponse.getDevices();
+
+		int i = 0;
+		for(Device dev : devices){
+			System.out.println("Available device "+ ++i+": " + dev.getId());
+		}
+		
+		return devices;
+	}
+
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
